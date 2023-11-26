27c79a5546b5130805569002f86a2b9e7cb63e77
mozdemir19 <mozdemir19@ku.edu.tr>
2023-11-26 23:22:06 +0300

    taskgroup constraint left

--- modified/realData.py
+++ modified/realData.py
@@ -8,15 +8,12 @@ import pickle
 
 min_bucket=1
 
-
-
 tasks = pd.read_csv('modified_data.csv')
 resources = pd.read_csv('resources.csv')
 serviceTypeDict = {'J': 'Normal_Service',
                    'A': 'Cargo/Mail'}
 
 print(tasks)
-
 
 tasks['Start_DateTime'] = pd.to_datetime(tasks.StartDate + " " + tasks.StartTime)
 
@@ -44,7 +41,7 @@ for index, row in tasks.iterrows():
     #resourceArrays = [aircraftAndTaskType, arrivalCat, departureCat, arrivalService, departureService]
     compatabilities[row.TaskId] = reduce(np.intersect1d, (aircraftAndTaskType, arrivalCat, departureCat, arrivalService, departureService)).tolist()
 
-
+print(compatabilities)
 time_series = pd.Series(True, index= pd.date_range(
         start=tasks.Start_DateTime.min(),
         end=tasks.End_DateTime.max(),
@@ -58,11 +55,6 @@ tasksHeatmap = tasks.apply(trunc_ts, axis=1).T
 tasksHeatmap[tasksHeatmap == True] = 1
 tasksHeatmap = tasksHeatmap.fillna(0).astype(int)
 tasksHeatmap.columns = tasks.TaskId.values
-
-tasksHeatmap['tot'] = tasksHeatmap.sum(axis=1)
-tasksHeatmap = tasksHeatmap[tasksHeatmap.tot > 1]
-tasksHeatmap.drop(['tot'], axis=1, inplace=True)
-
 tasksHeatmap = tasksHeatmap.drop_duplicates()
 
 
@@ -73,29 +65,68 @@ problem = LpProblem('Adnan_Menderes', LpMaximize)
 
 x = {}
 U = {}
+np.random.seed(1234)
 
+### DEFINE VARIABLES, only for compatible task-resource pairs
 for t in task_list:
     for r in compatabilities[t]:
-        x[t, r] = LpVariable("t%i_r%s" % (t, r), 0, 1, LpBinary)
+        x[t, r] = LpVariable("t%i_r%s" % (t, r), lowBound=0, upBound=1, cat=LpBinary)
         U[t, r] = np.random.randint(1, 10)
         
-problem += lpSum([U[t, r] * x[t, r] for t in task_list for r in compatabilities[t]])
+### DEFINE OBJECTIVE FUNCTION, over existing variables
+problem += lpSum([U[var] * x[var] for var in x])
 
-   
+### DEFINE CONSTRAINTS
+# i) each task is assigned to maximum of one resource
+for t in task_list:
+    if len(compatabilities[t]) > 0:
+        problem += lpSum([x[t, r] for r in compatabilities[t]]) <= 1
+        print(lpSum([x[t, r] for r in compatabilities[t]]))
+    
+
+# ii) assign only one task to a resource per time-step
 for idx, row in tasksHeatmap.iterrows():
-    # Get all the turns for time-bucket
-    turns_in_time_bucket = set(dict(row[row==1]).keys())
+    # Get all the tasks in time-step
+    tasks_in_time_step = set(dict(row[row==1]).keys())
+    #print(tasks_in_time_step)
     # For all gates
     for r in resource_list:
         # Constraints may be blank
-        cons = [x[t, r] for t in turns_in_time_bucket if (t, r) in x]
+        cons = [x[t, r] for t in tasks_in_time_step if (t, r) in x]
+        
         # Only need to impose constraint if there is an overlap
         if len(cons) > 1:
+            print(lpSum(cons))
+            #print(lpSum(cons))
             constraint_for_time_bucket = lpSum(cons) <= 1
             # These will occur when the plane overlaps change
             problem += constraint_for_time_bucket
+
 problem.solve()
 
+total = 0
+print('TaskId', '\t', 'ResourceId')
+assignments = pd.DataFrame(columns=['TaskId', 'ResourceId'])
+for var in x:
+    #print(x[var].varValue)
+    if x[var].varValue == 1:
+        
+        #new_row = pd.DataFrame({'TaskId': var[0], 'ResourceId':var[1]})
+        assignments.loc[len(assignments)] = [var[0], var[1]]
+        total += 1
+        #print(x[var].varValue)
+        #print(var[0], var[1])
+    
+
+
+print(assignments)
+print(total)
+lencompat = 0
+for var in compatabilities:
+    if len(compatabilities[var]) > 0:
+        lencompat += 1
+
+print(lencompat)
 
 
 #print(compatabilities)
