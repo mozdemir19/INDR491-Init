81c7f006949e0ac1d3db45626175adaa8ec9a54e
Mehmet Özdemir <memo@pop-os.localdomain>
2023-12-07 15:58:13 +0300

    hmmm

--- modified/realData.py
+++ modified/realData.py
@@ -61,6 +61,10 @@ tasksHeatmap = tasksHeatmap.drop_duplicates()
 task_list = tasks.TaskId.values
 resource_list = resources.ResourceId.values
 
+priorityGroupThree = tasks[tasks['AircraftTypeCode'] == 'F']
+priorityGroupThreeTaskList = priorityGroupThree.TaskId.values
+print(priorityGroupThree)
+
 problem = pl.LpProblem('Adnan_Menderes', pl.LpMaximize)
 
 x = {}
@@ -71,17 +75,20 @@ np.random.seed(1234)
 for t in task_list:
     for r in compatabilities[t]:
         x[t, r] = pl.LpVariable("t%i_r%s" % (t, r), lowBound=0, upBound=1, cat=pl.LpBinary)
-        U[t, r] = np.random.randint(1, 10)
-        
+        U[t, r] = 1 #np.random.randint(1, 10)
+
+
+print(priorityGroupThree.shape[0] - pl.lpSum([x[(t, r)] for t in priorityGroupThreeTaskList for r in compatabilities[t]]))
+
 ### DEFINE OBJECTIVE FUNCTION, over existing variables
-problem += pl.lpSum([U[var] * x[var] for var in x])
+problem += pl.lpSum([U[var] * x[var] for var in x]) - (priorityGroupThree.shape[0] - pl.lpSum([x[(t, r)] for t in priorityGroupThreeTaskList for r in compatabilities[t]]))
 
 ### DEFINE CONSTRAINTS
 # i) each task is assigned to maximum of one resource
 for t in task_list:
     if len(compatabilities[t]) > 0:
         problem += pl.lpSum([x[t, r] for r in compatabilities[t]]) <= 1
-        print(pl.lpSum([x[t, r] for r in compatabilities[t]]))
+        #print(pl.lpSum([x[t, r] for r in compatabilities[t]]))
     
 
 # ii) assign only one task to a resource per time-step
@@ -96,17 +103,17 @@ for idx, row in tasksHeatmap.iterrows():
         
         # Only need to impose constraint if there is an overlap
         if len(cons) > 1:
-            print(pl.lpSum(cons))
+            #print(pl.lpSum(cons))
             #print(lpSum(cons))
             constraint_for_time_bucket = pl.lpSum(cons) <= 1
             # These will occur when the plane overlaps change
             problem += constraint_for_time_bucket
 
+# iii) assign 
 
 problem.solve()
 
 total = 0
-print('TaskId', '\t', 'ResourceId')
 assignments = pd.DataFrame(columns=['TaskId', 'ResourceId'])
 for var in x:
     #print(x[var].varValue)
@@ -119,18 +126,16 @@ for var in x:
         #print(var[0], var[1])
     
 
+print(priorityGroupThree.shape[0] - pl.lpSum([x[(t, r)].varValue for t in priorityGroupThreeTaskList for r in compatabilities[t]]))
 
 print(assignments)
 assignments.to_csv('assignments.csv', index=False)
-print(total)
 lencompat = 0
 for var in compatabilities:
     if len(compatabilities[var]) > 0:
         lencompat += 1
 
-print(lencompat)
 
-print(assignments.loc[assignments['ResourceId'] == 6])
 gantt_df = pd.DataFrame({'ResourceId': assignments.ResourceId.values, 'TaskId': assignments.TaskId.values, 
                          'Duration': (tasks.loc[assignments.TaskId.values - 1].End_DateTime - tasks.loc[assignments.TaskId.values - 1].Start_DateTime).values,
                          'StartDateTime': tasks.loc[assignments.TaskId.values - 1].Start_DateTime.values})
